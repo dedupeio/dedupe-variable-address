aeca9b5290f7f267ab7655a97f9793529777feb9
Forest Gregg <fgregg@uchicago.edu>
2015-11-20 16:11:44 -0600

    normalize street directions, only compare po box ids

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError :
 setup(
     name='dedupe-variable-address',
     url='https://github.com/datamade/dedupe-variables-address',
-    version='0.0.4',
+    version='0.0.5',
     description='Address variable type for dedupe',
     packages=['dedupe.variables'],
     install_requires=['usaddress', 'parseratorvariable >= 0.0.10', 'future'],
