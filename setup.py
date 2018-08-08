57c61635cf7560652ed0597c41f77be85830ae6a
Forest Gregg <fgregg@users.noreply.github.com>
2018-08-08 13:24:05 -0500

    we don't use Extension in this setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 try:
-    from setuptools import setup, Extension
+    from setuptools import setup
 except ImportError :
     raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
 
