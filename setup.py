24ace528229388c439e643363c7827856fcfe001
Forest Gregg <fgregg@datamade.us>
2024-06-27 09:55:01 -0400

    long description

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 try:
     from setuptools import setup
-except ImportError :
-    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+except ImportError:
+    raise ImportError(
+        "setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools"
+    )
 
 
 setup(
-    name='dedupe-variable-address',
-    url='https://github.com/datamade/dedupe-variables-address',
-    version='1.0.0',
-    description='Address variable type for dedupe',
-    packages=['addressvariable'],
-    install_requires=['usaddress',
-                      'parseratorvariable >= 1.0.0'],
-    license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
-    )
+    name="dedupe-variable-address",
+    url="https://github.com/datamade/dedupe-variables-address",
+    version="1.0.0",
+    description="Address variable type for dedupe",
+    packages=["addressvariable"],
+    install_requires=["usaddress", "parseratorvariable >= 1.0.0"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="The MIT License: http://www.opensource.org/licenses/mit-license.php",
+)
